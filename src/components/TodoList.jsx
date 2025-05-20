f4c404d9d35aa08dc3c4619bbdbee14e2cbad2e2
VsalCode <vsal.ilham@gmail.com>
2025-05-20 15:25:14 +0700

    feat: add some adjusment

--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -1,4 +1,4 @@
-import React, { useContext } from "react";
+import { useContext } from "react";
 import { TodoContext } from "./TodoContext";
 
 const TodoList = () => {
@@ -11,14 +11,12 @@ const TodoList = () => {
   return (
     <ul className="w-full max-w-150">
       {todos.map((todo, index) => (
-        <li className="bg-[#202020] flex items-center justify-between rounded-xl h-fit py-4 px-5 mt-5" key={index}>
-          <div className="flex gap-5 items-center">
-            <input type="checkbox" name="checkbox" id="checkbox" />
-            <label className="text-xl" htmlFor="checkbox">
-              {todo}
-            </label>
-          </div>
-          <div className="flex gap-5 items-center">
+        <li className="bg-[#202020] flex items-center justify-between rounded-xl h-fit py-4 px-5 mt-5 " key={index}>
+          <label class="text-xl flex gap-5 items-center grow cursor-pointer">
+            <input type="checkbox" name="checkbox" id="checkbox" className="peer" />
+            <span className="peer-checked:line-through">{todo}</span>
+          </label>
+          <div className="flex gap-5 items-center ">
             <button className="bg-[#7D0A0A] px-4 py-2 rounded-xl cursor-pointer" onClick={() => handleDelete(index)}>
               Delete
             </button>
