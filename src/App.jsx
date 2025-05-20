622720fb2df2d0001149eb8c73749363d8916744
VsalCode <vsal.ilham@gmail.com>
2025-05-20 08:59:31 +0700

    init: project

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Homepage from "./pages/Homepage";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Homepage/>
+  }
+]);
+
+const App = () => {
+  return <RouterProvider router={router} />;
+};
+
+export default App;
